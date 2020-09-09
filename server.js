4af1a48bd49e9f38bd24b1e85553dcadeae9e0fd
Iza Witkowska <isa.witkowska@gmail.com>
2020-09-09 18:04:41 +0200

    Add information about login

--- server.js
+++ server.js
@@ -24,14 +24,12 @@ app.get('/about', (req, res) => {
   res.show('about.html');
 });
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/user/${name}`));
-  };
-  next();
+app.get('/user/settings', (req, res) => {
+  res.show('login.html');
 });
-app.get('/user/', (req, res) => {
-  res.show('panel.html');
+
+app.get('/user/panel', (req, res) => {
+  res.show('login.html');
 });
 
 app.use((req, res) => {
